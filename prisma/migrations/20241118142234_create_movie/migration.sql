dd937643cd35a72baaeb663389d1f8f2d72846d3
mdmdm1 <medmahmoudd1@gmail.com>
2024-11-18 16:56:54 +0000

    Create the movie model

---
+++ prisma/migrations/20241118142234_create_movie/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE `Movie` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(255) NOT NULL,
+    `description` TEXT NULL,
+    `status` ENUM('TO_WATCH', 'IN_PROGRESS', 'WATCHED') NOT NULL DEFAULT 'WATCHED',
+    `AddedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
