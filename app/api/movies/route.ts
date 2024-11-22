0632d5855c0eeecff3fc718f2a716366448430e1
mdmdm1 <medmahmoudd1@gmail.com>
2024-11-22 10:16:14 +0000

    Build an api for creating watched movies

---
+++ app/api/movies/route.ts
@@ -0,0 +1,22 @@
+import prisma from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+import {z} from "zod";
+
+
+const createMovieSchema = z.object({
+    title: z.string().min(1).max(255)
+})
+export async function POST(request: NextRequest)
+{
+    const body = await request.json();
+    const validation =createMovieSchema.safeParse(body);
+    if(!validation.success)
+        return NextResponse.json(validation.error.errors, {status: 400})
+
+    const newMovie= await prisma.movie.create({
+    data:{title: body.title} 
+    });
+
+    return NextResponse.json(newMovie, {status: 201});
+    
+}
